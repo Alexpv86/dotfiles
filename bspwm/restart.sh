013410472977d004dce9b8a048567af329d0055e
Alex <>
2024-01-02 15:28:30 +0500

    Init

---
+++ bspwm/restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# sxhkd
+pkill -USR1 -x sxhkd
+
+# bspc
+bspc wm -r
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# bspwm
+bash bspwm/install.sh
+bash bspwm/restart.sh
+
+# polybar
+bash polybar/install.sh
+
+# picom
+bash picom/install.sh
