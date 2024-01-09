7d6d2e9c729424fc30f3e5d96358007e47a13522
Alex <>
2024-01-09 19:33:29 +0500

    Исправил опечатки

--- kmonad/etc/kmonad/kmonad_run.sh
+++ kmonad/etc/kmonad/kmonad_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ -e "/dev/input/by-id/platform-i8042-serio-o-event-kbd" ]]; then
+if [[ -e "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ]]; then
     /sbin/kmonad "/etc/kmonad/gaming_kb_vb.kbd"
 elif [[ -e "/dev/input/by-id/usb-Gaming_KB_Gaming_KB-event-kbd" ]]; then
     /sbin/kmonad "/etc/kmonad/gaming_kb.kbd"
 fi
+
+exit 1
