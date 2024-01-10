0bf5b18a569d00a008671caf7fabcf1d7fe9019f
Alex <>
2024-01-10 19:54:31 +0500

    подправил конфиг для kmonad

--- keyboard/copy_config.sh
+++ keyboard/copy_config.sh
@@ -2,4 +2,4 @@
 
 DIR=$(dirname "$0")
 sudo cp "$DIR/00-keyboard.conf" /etc/X11/xorg.conf.d/
-sudo cp "$DIR/group" /usr/share/X11/xkb/symbols/
+#sudo cp "$DIR/group" /usr/share/X11/xkb/symbols/
--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-paru -S neovim --noconfirm --needed
+paru -S neovim xclip --noconfirm --needed
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
