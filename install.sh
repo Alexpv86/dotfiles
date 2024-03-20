4c6bb2c4f34443f11947fa17e8e75a71de20d1dc
typewria <typewria@student.21-school.ru>
2024-03-20 15:28:06 +0500

    install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+mainpkg=(
+	thunar
+	stow
+)
+
+for pkg in ${mainpkg[@]}; do
+	if [ ! paru -Qi ${pkg} ] &>/dev/null; then
+		paru -S ${pkg} --noconfirm &>/dev/null
+
+		if [ ! paru -Qi ${pkg} ] &>/dev/null; then
+			echo "${pkg} is installed"
+		else
+			echo "ERROR: ${pkg} failed to install"
+		fi
+	else
+		echo "${pkg} is already installed"
+	fi
+done
