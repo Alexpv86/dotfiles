2c0e781e5c10e60fe92652747c61f1727673374d
typewria <typewria@student.21-school.ru>
2024-03-21 17:56:14 +0500

    Add install progs

--- install.sh
+++ install.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
+if [[ ! $(command -v paru) ]]; then
+	git clone https://aur.archlinux.org/paru-bin.git
+	cd paru-bin || exit
+	makepkg -si
+	cd ..
+	rm -rf paru-bin
+fi
+
 mainpkg=(
+	# Fonts
+	ttf-jetbrains-mono
+	ttf-jetbrains-mono-nerd
+	ttf-nerd-fonts-symbols
+	awesome-terminal-fonts
+
+	# System
+	lxappearance
+	qt5ct
+	man-db
+	xorg-xrandr
+
+	picom
+	rofi # menu
+	polybar
+	alacritty # terminal
+	stow      # config manager
+
+	# Neovim
+	neovim
+	xclip
+	ripgrep
+	fd
+	jq
+
+	# Thunar (explorer)
 	thunar
-	stow
+	thunar-volman
+	thunar-archive-plugin
+	thunar-media-tags-plugin
+  gvfs
+	tumbler
 )
 
 for pkg in ${mainpkg[@]}; do
 	paru -Qi ${pkg} &>/dev/null
 
 	if [[ $? == 1 ]] &>/dev/null; then
-		paru -S ${pkg} --noconfirm &>/dev/null
+		paru -S ${pkg} --noconfirm --needed &>/dev/null
 
 		paru -Qi ${pkg} &>/dev/null
 		if [[ $? == 0 ]] &>/dev/null; then
