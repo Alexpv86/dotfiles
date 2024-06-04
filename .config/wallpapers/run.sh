6d5ea37707a8d60bc88c9919a7b1e0a7b4606729
typewria <typewria@student.21-school.ru>
2024-06-04 23:10:59 +0500

    wallpapers

--- .config/wallpapers/run.sh
+++ .config/wallpapers/run.sh
@@ -2,6 +2,7 @@
 
 while [[ true ]]; do
 	sleep 1
-	nitrogen --random --set-centered
+	# nitrogen --random --set-centered
+	feh --bg-scale --no-fehbg -z -r $HOME/Documents/Wallpapers
 	sleep 360
 done
