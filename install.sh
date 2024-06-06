c9a89ac653c004ef96728480b3622d486e20296a
typewria <typewria@student.21-school.ru>
2024-06-06 18:00:03 +0500

    Add progs to sudoers

--- install.sh
+++ install.sh
@@ -102,8 +102,13 @@ git config --global user.email "typewria@student.21-school.ru"
 git config --global user.name "typewria"
 
 # localectl --no-convert set-x11-keymap us,ru
+
 export SUDO_FILE="/etc/sudoers.d/00_$USER"
 export SUDO_STR="$USER ALL= NOPASSWD: /usr/bin/systemctl suspend"
+sudo grep -qF "$SUDO_STR" "$SUDO_FILE" || sudo -E bash -c 'echo "$SUDO_STR" >> $SUDO_FILE'
+SUDO_STR="$USER ALL= NOPASSWD: /usr/bin/wg-quick"
+sudo grep -qF "$SUDO_STR" "$SUDO_FILE" || sudo -E bash -c 'echo "$SUDO_STR" >> $SUDO_FILE'
+SUDO_STR="$USER ALL= NOPASSWD: /usr/bin/reboot"
 sudo grep -qF "$SUDO_STR" "$SUDO_FILE" || sudo -E bash -c 'echo "$SUDO_STR" >> $SUDO_FILE'
 
 cp -rf .local/share/* ~/.local/share/
