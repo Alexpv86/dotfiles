8611d17be66622b48974135f4e93f93e6ece7ac8
typewria <typewria@student.21-school.ru>
2024-06-11 01:12:49 +0500

    Refactoring

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -5,12 +5,15 @@ BASE_DIR="$(realpath $(dirname ${0}))"
 export ROOT_DIR="${BASE_DIR}/files"
 export HOME_DIR="${ROOT_DIR}/home"
 export CONFIG_DIR="${HOME_DIR}/.config"
+
 export STOW_IGNORE_FILE="${HOME_DIR}/.stow-local-ignore"
+echo -n > ${STOW_IGNORE_FILE}
 
 scripts=($(ls -d ${BASE_DIR}/scripts/*.sh))
 for i in "${!scripts[@]}"; do
 	bash "${scripts[i]##}"
 done
 
+echo ".local" >> $STOW_IGNORE_FILE
 cd ${HOME_DIR}
 stow -R -t ${HOME} .
