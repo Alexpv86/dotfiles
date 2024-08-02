bd4676a5eef5260101a630c779934c9457e86909
typewria <typewria@student.21-school.ru>
2024-08-02 17:54:21 +0500

    .

--- files/root/home/.config/wallpapers/get_bing.sh
+++ files/root/home/.config/wallpapers/get_bing.sh
@@ -3,7 +3,7 @@ folder="$HOME/wallpapers"
 
 BACKGROUND_META=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
 filename=$(echo $BACKGROUND_META | jq -r '.images[0].urlbase')
-filename="${folder}/${filename##*.}.jpg"
+filename="${folder}/${filename##*.}_$(date +%Y%m%d).jpg"
 
 if ! [[ -f ${filename} ]]; then
 	curl -o ${filename} "https://bing.com$(echo $BACKGROUND_META | jq -r '.images[0].url')"
