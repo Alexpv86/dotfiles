070f27f4f85e256d14a66648c5cbd728fb573d03
typewria <typewria@student.21-school.ru>
2024-08-04 21:59:49 +0500

    add auto commit, pull and bush wallpapers

--- files/root/home/.config/wallpapers/get_bing.sh
+++ files/root/home/.config/wallpapers/get_bing.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-folder="$HOME/wallpapers"
+
+cd "$HOME/wallpapers"
 
 BACKGROUND_META=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
 filename=$(echo $BACKGROUND_META | jq -r '.images[0].urlbase')
-filename="${folder}/${filename##*.}_$(date +%Y%m%d).jpg"
+filename="${filename##*.}_$(date +%Y%m%d).jpg"
 
 if ! [[ -f ${filename} ]]; then
 	curl -o ${filename} "https://bing.com$(echo $BACKGROUND_META | jq -r '.images[0].url')"
+fi
+
+if [[ $(git status --porcelain) ]]; then
+	git pull
+	git add ${filename}
+	git commit -m "auto commit"
+	git push
 fi
 
 feh --bg-fill --no-fehbg "${filename}"
