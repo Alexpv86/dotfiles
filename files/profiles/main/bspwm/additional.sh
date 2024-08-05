bd943e884f27a306bafa63632e5064f36a2cd913
typewria <typewria@student.21-school.ru>
2024-08-05 19:18:23 +0500

    .

--- files/profiles/main/bspwm/additional.sh
+++ files/profiles/main/bspwm/additional.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-xrandr --output DVI-D-0 --left-of HDMI-0
+mon_list=$(xrandr --listactivemonitors | tail -n +2 | awk {'print $4'})
+mon_count=$(echo ${mon_list} | wc -l)
+mon_primary=$(echo ${mon_list} | rofi -dmenu)
 
+xrandr --output ${mon_primary} --primary
 polybar -r center &
-polybar -r left &
+bspc monitor ${mon_primary} -d   󰖟 1 2 3
+
+# if [[ ${mon_count} > 1 ]]; then
+#   xrandr --output DVI-D-0 --left-of HDMI-0
+#   polybar -r left &
+#   bspc monitor DVI-D-0 -d 4 5 6
+# fi
 #polybar -r right &
 
-bspc monitor HDMI-0 -d   󰖟 1 2 3
-bspc monitor DVI-D-0 -d 4 5 6
 # bspc monitor DVI-I-1 -d  8 9
