876b6b742059bd2d3066cde7d2248fc84e55a7ec
typewria <typewria@student.21-school.ru>
2024-09-23 02:53:50 +0500

    nvim-cmp: Add functions for select next and prev kind

--- files/root/home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ files/root/home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,71 @@
+local cmp = require("cmp")
+
+-- Функция для перехода к предыдущей записи с другим 'kind'
+local function select_prev_kind()
+  -- Получаем текущую выбранную запись
+  local current_entry = cmp.get_selected_entry()
+
+  if not current_entry then
+    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+    return
+  end
+
+  local current_kind = current_entry.completion_item.kind
+
+  cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+  local prev_entry = cmp.get_selected_entry()
+
+  if not prev_entry or prev_entry.completion_item.kind ~= current_kind then
+    if not prev_entry then
+      cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+    end
+    current_entry = cmp.get_selected_entry()
+    current_kind = current_entry.completion_item.kind
+  end
+
+  for _ = 1, #cmp.get_entries() do
+    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+    prev_entry = cmp.get_selected_entry()
+
+    -- Проверяем, отличается ли категория (kind) нового элемента
+    if not prev_entry or prev_entry.completion_item.kind ~= current_kind then
+      cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      return
+    end
+  end
+end
+
+-- Функция для перехода к следующей записи с другим 'kind'
+local function select_next_kind()
+  -- Получаем текущую выбранную запись
+  local current_entry = cmp.get_selected_entry()
+
+  if not current_entry then
+    -- Если ничего не выбрано, просто выбираем первый элемент
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    return
+  end
+
+  -- Текущий 'kind' (категория) выбранной записи
+  local current_kind = current_entry.completion_item.kind
+
+  -- Начинаем с текущей записи и ищем первую с другим 'kind'
+  for _ = 1, #cmp.get_entries() do
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    local next_entry = cmp.get_selected_entry()
+
+    if not next_entry then
+      cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      return
+    end
+
+    -- Проверяем, отличается ли категория (kind) нового элемента
+    if next_entry.completion_item.kind ~= current_kind then
+      return
+    end
+  end
+end
+
 return {
   {
     "nvim-cmp",
@@ -11,7 +79,6 @@ return {
       -- opts.experimental = opts.experimental or {}
       opts.experimental.ghost_text = false
 
-      local cmp = require("cmp")
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-Space>"] = function(fallback)
           if cmp.visible() then
@@ -21,6 +88,12 @@ return {
           end
         end,
         ["<CR>"] = LazyVim.cmp.confirm({ select = true }),
+        ["<Right>"] = function()
+          select_next_kind()
+        end,
+        ["<Left>"] = function()
+          select_prev_kind()
+        end,
       })
     end,
   },
